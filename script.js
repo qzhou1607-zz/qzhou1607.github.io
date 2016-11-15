b5ed359e3ca8f212acd2e17188bf9789f95ca606
Qing Zhou <qzhou.pku@gmail.com>
2016-11-15 03:51:04 +0000

    more edits

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    //typed animation effect
+    $(function(){
+        $("#description").typed({
+            strings: ["A Full-Stack Developer^500", "A Microfluidics Engineer^500", "A Quick Learner^500"],
+            typeSpeed: 80,
+            loop:true
+        });
+    });
+    
+    //smooth scroll
+    var $root = $('html,body');
+    $('a[href*="#"]').on('click', function() {
+        var self = $(this);
+        //alert($(self.attr("href")));
+        $root.animate({
+            scrollTop:$(self.attr("href")).offset().top
+        },1000);
+        return false;
+    });
+});
